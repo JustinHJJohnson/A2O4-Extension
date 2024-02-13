5e912aceec6b39beeacb0ef92d697954560192c9
Justin Johnson <justin.h.j.johnson@gmail.com>
2024-02-13 22:10:05 +1100

    initial commit

---
+++ download.js
@@ -0,0 +1,72 @@
+async function downloadUrl(AO3Url) {
+    let res = await browser.storage.local.get();
+    serverIp = `${res.AO3_ip}:${res.AO3_port}`
+    
+    download_button = document.getElementById("downloadAPIText")
+    info_dropdown = document.getElementById("dropdownInfo")
+    info_dropdown_text = document.getElementById("dropdownInfoText")
+    
+    download_button.text = "Downloading"
+    
+    let response
+    try {
+        response = await fetch(`http://${serverIp}/download`, {
+          	method: "POST",
+          	headers: {
+            	'Content-Type': 'application/json'
+          	},
+          	body: JSON.stringify({
+            	url: AO3Url
+          	}),
+        });
+        console.log(await response);
+    } catch (e) {
+        console.error(e);
+        download_button.text = "Failed"
+        return
+    }
+
+    switch (response.status) {
+        case 503:
+            response_text = await response.text()
+            download_button.text = "Failed"
+            info_dropdown_text.innerText = response_text
+            info_dropdown.classList.remove("hidden")
+            break;
+        default:
+            download_button.text = "Success"
+            break;
+    }
+}
+
+if (!document.URL.includes("search")) {
+    console.log("Adding API download button")
+    navigationButtons = document.getElementsByClassName("navigation actions")[2]
+    const newButton = document.createElement("li")
+    const newButtonText = document.createElement("a")
+    const newButtonDropdown = document.createElement("ul")
+    const newButtonDropdownInfo = document.createElement("li")
+    const newButtonDropdownInfoText = document.createElement("a")
+
+    newButtonText.text = "A2O4"
+    newButtonText.id = "downloadAPIText"
+
+    newButtonDropdown.className = "expandable secondary hidden"
+    newButtonDropdown.id = "dropdownInfo"
+    newButtonDropdown.style.marginTop = "36px"
+    
+    newButtonDropdownInfoText.id = "dropdownInfoText"
+    newButtonDropdownInfoText.style.background = "none"
+    newButtonDropdownInfoText.style.border = "none"
+
+    newButtonDropdownInfo.appendChild(newButtonDropdownInfoText)
+    newButtonDropdown.appendChild(newButtonDropdownInfo)
+    newButton.appendChild(newButtonText)
+    //newButton.appendChild(document.createTextNode("\u00a0"))
+    newButton.appendChild(newButtonDropdown)
+    newButton.addEventListener("click", (event) => downloadUrl(document.URL))
+    navigationButtons.appendChild(newButton)
+} else {
+    console.log("Wrong Page")
+}
+
---
+++ popup/AO3.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#address");
+
+async function sendData() {
+  const formData = new FormData(form);
+
+  await browser.storage.local.set({
+    AO3_ip: formData.get("ip"),
+    AO3_port: formData.get("port")
+  });
+}
+
+// Take over form submission
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+});
+
+async function restoreConnectionDetails() {
+    let res = await browser.storage.local.get();
+    document.querySelector("#ip").value = res.AO3_ip || ''
+    document.querySelector("#port").value = res.AO3_port || ''
+}
+
+document.addEventListener('DOMContentLoaded', restoreConnectionDetails);
